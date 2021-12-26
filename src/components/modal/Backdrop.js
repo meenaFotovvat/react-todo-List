9528772653eed546e3fd2f8b56022faed4493f0b
meenaFotovvat <meenafotovvat@gmail.com>
2021-12-26 13:46:13 +0330

    chore: remove useless codes

--- src/components/modal/Backdrop.js
+++ src/components/modal/Backdrop.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-import classes from './Backdrop.module.scss';
+import classes from "./Backdrop.module.scss";
 
-
-function Backdrop(props){
-    return(
-        <div className={classes["bg-task-page"]}  onClick={props.onCancel} data-display-task />
-    )
+function Backdrop(props) {
+  return <div className={classes["bg-task-page"]} onClick={props.onCancel} />;
 }
 
 export default Backdrop;
